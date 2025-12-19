dbacdf42098b5d482159604942709c32fb1825d6
Tapekhin Aleksey <gray@yandex.ru>
2025-12-19 11:50:09 +0000

    запущен метрикс

--- delcont.sh
+++ delcont.sh
@@ -5,3 +5,15 @@ docker ps -qa | xargs docker rm -f
 docker images -qa | xargs docker rmi -f
 sudo docker system prune -f --volumes
 docker volume prune -f
+
+sudo rm -rf /etc/graylog/*
+
+echo "удалена директория грейлог"
+
+docker volume rm \
+  server_mongodb_data \
+  server_mongodb_config \
+  server_graylog-datanode \
+  server_graylog_data
+
+
